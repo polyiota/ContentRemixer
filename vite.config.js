9a51358e02cacd6727452999b3710db1d28db95e
Daniel Hatke <dh@polyiota.com>
2025-01-09 13:26:25 -0700

    github pages fixes

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your actual repository name
+  base: mode === 'production' ? '/ContentRemixer/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true
   }
-}) 
+})) 
