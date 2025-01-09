9017a51760ce7202de5d4e4f80d8d0e43e102b62
Daniel Hatke <dh@polyiota.com>
2025-01-09 14:48:33 -0700

    GHPages changes

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/ContentRemixer/' : '/',
+  base: '/ContentRemixer/',
   build: {
     outDir: 'dist',
     sourcemap: true
   }
-})) 
+}) 
